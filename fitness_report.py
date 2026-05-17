192eaf86902030eb44ff3027e359609a5692bafc
fitness-dashboard <671999@mailbox.org>
2026-05-17 10:36:36 +0200

    Initial fitness dashboard project

--- fitness_report.py
+++ fitness_report.py
@@ -11,12 +11,20 @@ import sys
 from load_data import lade_daten
 from calculations import berechne_werte
 from excel_export import exportiere_excel
-
+from charts import erstelle_diagramme
 
 print("Programm gestartet")
 
 
 dfGesamt = lade_daten()
 dfGesamt = berechne_werte(dfGesamt)
-exportiere_excel(dfGesamt)
+
+# CSV-Datei exportieren für Export nach Github
+dfGesamt.to_csv(
+    "fitness_dashboard_data.csv",
+    index=False
+)
+
+workbook = exportiere_excel(dfGesamt)
+erstelle_diagramme(workbook, dfGesamt)
 
