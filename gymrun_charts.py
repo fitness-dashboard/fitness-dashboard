e755c62e855a2330d327953012739c07a43dde78
fitness-dashboard <671999@mailbox.org>
2026-06-13 17:47:23 +0200

    Automatisches Dashboard Update

--- gymrun_charts.py
+++ gymrun_charts.py
@@ -156,7 +156,7 @@ def create_rm_excel_chart(
 
     chart = wsGymRunCharts.charts.add()
 
-    chart.chart_type = "line"
+    chart.chart_type = "xy_scatter_lines"
 
     # chart.set_source_data(
     #     wsGymRun.range(
