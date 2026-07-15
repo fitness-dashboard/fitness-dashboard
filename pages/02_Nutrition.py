0a5ea483e37b321e095d0d6da330cca0be851607
fitness-dashboard <671999@mailbox.org>
2026-07-15 19:39:34 +0200

    Automatisches Dashboard Update

---
+++ pages/02_Nutrition.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("🍎 Nutrition")
+
+st.write("Willkommen auf der Nutrition-Seite.")
---
+++ pages/03_Body.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("⚖️ Body")
+
+st.write("Willkommen auf der Body-Seite.")
